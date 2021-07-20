d0bf043c86feba9105a649d005b6b9a0431f1e62
Niyrme <niyrme@niyrme.xyz>
2021-07-20 22:58:38 +0200

    Attempt 2: test if test failure is because of how DbConnector is used

--- tests/test_Commands.py
+++ tests/test_Commands.py
@@ -9,34 +9,32 @@ from commands import (
 )
 from db import DbConnector
 
-connector = DbConnector(is_test=True)
-
 
 def test_SayHelloCommand():
-    cmd = SayHelloCommand(connector, "DataFrittata")
+    cmd = SayHelloCommand(DbConnector(is_test=True), "DataFrittata")
     assert cmd.run() == "Welcome to the stream, DataFrittata"
     assert cmd.is_restricted is False
 
 
 def test_ListCommandsCommand():
-    cmd = ListCommandsCommand(connector)
+    cmd = ListCommandsCommand(DbConnector(is_test=True))
     assert cmd.run() == "!hello !commands !today !settoday !bot !source !settsource !reloadcommands"
     assert cmd.is_restricted is False
 
 
 def test_TodayCommand():
-    cmd = TodayCommand(connector)
+    cmd = TodayCommand(DbConnector(is_test=True))
     assert cmd.run() == "today is not set yet 😭."
     assert cmd.is_restricted is False
 
 
 def test_BotCommand():
-    cmd = BotCommand(connector)
+    cmd = BotCommand(DbConnector(is_test=True))
     assert cmd.run() is None
     assert cmd.is_restricted is False
 
 
 def test_SourceCommand():
-    cmd = SourceCommand(connector)
+    cmd = SourceCommand(DbConnector(is_test=True))
     assert cmd.run() == "source is not set yet."
     assert cmd.is_restricted is False
