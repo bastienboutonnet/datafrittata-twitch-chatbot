24708f9bd825503da5ef38ebb3ddf7c23eaf7f1d
niyrme <53240445+niyrme@users.noreply.github.com>
2021-07-20 22:41:40 +0200

    Fix Tests for DB: Attempt 1
    First attempt to fix tests for new DB backend.

--- tests/test_Commands.py
+++ tests/test_Commands.py
@@ -7,33 +7,37 @@ from commands import (
     SourceCommand,
     TodayCommand,
 )
+from db import DbConnector
+
+
+connector = DbConnector(is_test=True)
 
 
 def test_SayHelloCommand():
-    cmd = SayHelloCommand("DataFrittata")
+    cmd = SayHelloCommand(connector, "DataFrittata")
     assert cmd.run() == "Welcome to the stream, DataFrittata"
     assert cmd.is_restricted is False
 
 
 def test_ListCommandsCommand():
-    cmd = ListCommandsCommand()
+    cmd = ListCommandsCommand(connector)
     assert cmd.run() == "!hello !commands !today !settoday !bot !source !settsource !reloadcommands"
     assert cmd.is_restricted is False
 
 
 def test_TodayCommand():
-    cmd = TodayCommand()
+    cmd = TodayCommand(connector)
     assert cmd.run() == "today is not set yet 😭."
     assert cmd.is_restricted is False
 
 
 def test_BotCommand():
-    cmd = BotCommand()
+    cmd = BotCommand(connector)
     assert cmd.run() is None
     assert cmd.is_restricted is False
 
 
 def test_SourceCommand():
-    cmd = SourceCommand()
+    cmd = SourceCommand(connector)
     assert cmd.run() == "source is not set yet."
     assert cmd.is_restricted is False
