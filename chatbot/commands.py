6f27c64aaae5f95afd31b748b906c386380a2a7f
Bastien Boutonnet <bastien.b1@gmail.com>
2021-07-31 19:37:41 +0200

    feat/fix: make db commands more robust and rewrite uptime to use twitch API (#22)
    * add try exepts, check we get results from db, rewrite uptime
    
    * add todo
    
    * update tests

--- chatbot/commands.py
+++ chatbot/commands.py
@@ -3,8 +3,10 @@ from abc import ABC
 from datetime import datetime
 from typing import Dict, List, Optional, Type
 
+import httpx
 from irc.client import ServerConnection
 
+from chatbot.config import Config
 from chatbot.db import DbConnector
 
 START_TIME = datetime.now()
@@ -15,8 +17,9 @@ def send_message(connection: ServerConnection, channel: str, text: str):
 
 
 class BaseCommand(ABC):
-    def __init__(self, db_connector: DbConnector, **kwargs):
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
         self.db_connector = db_connector
+        self.config = config
 
     @property
     def is_restricted(self):
@@ -27,20 +30,38 @@ class BaseCommand(ABC):
 
 
 class UptimeCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, **kwargs):
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
         self.db_connector = db_connector
+        self.config = config
 
     def run(self):
-        delta = (datetime.now() - START_TIME).seconds
-        hours = delta // 3600
-        minutes = (delta // 60) % 60
-        seconds = delta % 60
-        return f"We've been online for {hours} hours, {minutes} minutes and {seconds} seconds"
+        url = f"https://api.twitch.tv/helix/streams?user_login={self.config.channel}"
+        headers = {
+            "Authorization": f"Bearer {self.config.bot_api_token}",
+            "Client-ID": self.config.client_id_api,
+        }
+        response = httpx.get(url, headers=headers)
+        response_json = response.json()
+
+        # TODO: test this when we're live and write unit test with a httpx mock
+        if response_json.get("data", []):
+            start_time = response_json["data"][0]["started_at"]
+            start_time = datetime.strptime(
+                start_time,
+                "%Y-%m-%dT%H:%M:%SZ",
+            )
+            delta = (datetime.now() - start_time).seconds
+            hours = delta // 3600
+            minutes = (delta // 60) % 60
+            seconds = delta % 60
+            return f"We've been online for {hours} hours, {minutes} minutes and {seconds} seconds"
+        else:
+            return f"{self.config.channel} is not currently streaming"
 
 
 class SayHelloCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, user_name: str, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, user_name: str, **kwargs):
+        super().__init__(db_connector, config=config)
         self.user_name = user_name
 
     def run(self):
@@ -49,8 +70,8 @@ class SayHelloCommand(BaseCommand):
 
 
 class ListCommandsCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
+        super().__init__(db_connector, config)
 
     def run(self):
         all_commands = " !".join(AVAILABLE_COMMANDS)
@@ -59,8 +80,8 @@ class ListCommandsCommand(BaseCommand):
 
 
 class TodayCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
+        super().__init__(db_connector, config)
 
     def run(self):
         today_text = self.db_connector.retrive_command_response("today")
@@ -69,8 +90,8 @@ class TodayCommand(BaseCommand):
 
 
 class SetTodayCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, command_input: str, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, command_input: str, **kwargs):
+        super().__init__(db_connector, config)
         self.today_text = command_input
 
     @property
@@ -83,24 +104,24 @@ class SetTodayCommand(BaseCommand):
 
 
 class BotCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
+        super().__init__(db_connector, config)
 
     def run(self):
         return self.db_connector.retrive_command_response("bot")
 
 
 class SourceCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, **kwargs):
+        super().__init__(db_connector, config)
 
     def run(self):
         return self.db_connector.retrive_command_response("source")
 
 
 class SetSourceCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, command_input: str, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, command_input: str, **kwargs):
+        super().__init__(db_connector, config)
         self.source_text = command_input
 
     @property
@@ -112,8 +133,8 @@ class SetSourceCommand(BaseCommand):
 
 
 class SetUserCountryCommand(BaseCommand):
-    def __init__(self, db_connector: DbConnector, command_input: str, **kwargs):
-        super().__init__(db_connector)
+    def __init__(self, db_connector: DbConnector, config: Config, command_input: str, **kwargs):
+        super().__init__(db_connector, config)
         self.user_id = kwargs.get("user_id")
         self.user_country = command_input.lower()
 
@@ -128,7 +149,7 @@ class SetUserCountryCommand(BaseCommand):
 
 
 AVAILABLE_COMMANDS: Dict[str, Type[BaseCommand]] = {
-    "hello": SayHelloCommand,  # type: ignore
+    "hello": SayHelloCommand,
     "commands": ListCommandsCommand,
     "today": TodayCommand,
     "settoday": SetTodayCommand,
